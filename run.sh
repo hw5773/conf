2d75ba5e1d735e9a323b68022c3e960979dcb79f
Hyunwoo Lee <hw5773@gmail.com>
2020-10-04 16:34:08 -0400

    update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 cd ~
 sudo apt-get update && sudo apt-get install -y build-essential
+sudo ln -s python3 /usr/bin/python
 git clone https://hw5773:votmdnjem2020@github.com/hw5773/tls13measure.git
 cd tls13measure/performance
 ./perf.sh ~/logs email hwlee hw5773@gmail.com $1
