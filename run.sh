ee3dc4f86908cf8340736930c15d6bb8bff0921c
Hyunwoo Lee <hw5773@gmail.com>
2020-10-04 15:53:34 -0400

    update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cd ~
 sudo apt-get update && sudo apt-get install -y build-essential
 git clone https://hw5773:votmdnjem2020@github.com/hw5773/tls13measure.git
 cd tls13measure/performance
