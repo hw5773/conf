1bef301f00ca3f102349eb43e0eb128e4ecf773f
Hyunwoo Lee <hw5773@gmail.com>
2020-10-04 15:53:09 -0400

    add run.sh

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get install -y build-essential
+git clone https://hw5773:votmdnjem2020@github.com/hw5773/tls13measure.git
+cd tls13measure/performance
+./perf.sh ~/logs email hwlee hw5773@gmail.com $1
